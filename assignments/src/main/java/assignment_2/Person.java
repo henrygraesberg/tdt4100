55b2e691533a4c7121819daeebe8f5be013ffd34
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-01-21 13:41:09 +0100

    Pass set email tests

--- assignments/src/main/java/assignment_2/Person.java
+++ assignments/src/main/java/assignment_2/Person.java
@@ -53,22 +53,27 @@ public class Person {
     IllegalArgumentException argumentError = new IllegalArgumentException("Email must match the format firstname.lastname@domain.countrycode");
 
     String[] emailSplit = email.split("@");
-    boolean isTwoAts = emailSplit.length == 2;
+    boolean hasAt = emailSplit.length == 2;
 
-    if(!isTwoAts) throw argumentError;
+    if(!hasAt) throw argumentError;
 
-    String[] emailNameSplit = emailSplit[0].split(".");
+    System.out.println(emailSplit);
+
+    String[] emailNameSplit = emailSplit[0].split("\\.");
+    System.out.println(emailNameSplit);
     String[] personNameSplit = this.name.split(" ");
     boolean isTwoNames = emailNameSplit.length == 2;
-    boolean firstNameMatches = emailNameSplit[0].equals(personNameSplit[0]);
-    boolean lastNameMatches = emailNameSplit[1].equals(personNameSplit[1]);
 
-    if(!(isTwoNames && firstNameMatches && lastNameMatches))
-      throw argumentError;
+    if(!isTwoNames) throw new IllegalArgumentException();
+
+    boolean firstNameMatches = emailNameSplit[0].equals(personNameSplit[0].toLowerCase());
+    boolean lastNameMatches = emailNameSplit[1].equals(personNameSplit[1].toLowerCase());
+
+    if(!(firstNameMatches && lastNameMatches)) throw argumentError;
     
     String[] ccTLDs = {"ad", "ae", "af", "ag", "ai", "al", "am", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"};
 
-    String[] domainSplit = emailSplit[1].split(".");
+    String[] domainSplit = emailSplit[1].split("\\.");
     boolean hasTLD = false;
 
     for (String tld : ccTLDs) {
@@ -94,5 +99,18 @@ public class Person {
 
   public void setGender(char gender) {
     
+  }
+
+  public static void main(String[] args) {
+    Person p = new Person();
+
+    String email = "ola.nordmann@ntnu.no";
+    String[] emailSplit = email.split("@");
+    String[] nameSplit = emailSplit[0].split("\\.");
+    
+    System.out.println(emailSplit[0]);
+    System.out.println(nameSplit[0]);
+
+    p.setName("Ola Nordmann");
   }
 }
