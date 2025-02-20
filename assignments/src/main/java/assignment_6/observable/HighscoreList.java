1ce43542385c678034ce3e504eb7461eba171615
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-02-21 00:20:21 +0100

    Create HighscoreList and appropriate listener

---
+++ assignments/src/main/java/assignment_6/observable/HighscoreList.java
@@ -0,0 +1,59 @@
+package assignment_6.observable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HighscoreList {
+  private int maxSize;
+  private List<Integer> results = new ArrayList<Integer>(maxSize);
+  private List<HighscoreListListener> highscoreListListeners = new ArrayList<HighscoreListListener>();
+
+  public HighscoreList(int maxSize) {
+    if(maxSize <= 0) throw new IllegalArgumentException("Max size cannot be 0 or less");
+
+    this.maxSize = maxSize;
+  }
+
+  public int size() {
+    return this.results.size();
+  }
+
+  public int getElement(int index) {
+    return this.results.get(index);
+  }
+
+  public void addResult(int result) {
+    int oldSize = this.results.size();
+
+    Consumer<HighscoreListListener> notifyListener = listener -> listener.listChanged(this, result);
+
+    if(oldSize == 0) {
+      this.results.add(result);
+      highscoreListListeners.forEach(notifyListener);
+      return;
+    }
+
+    for (int i = 0; i < oldSize; i++) {
+      if(this.results.get(i) > result) this.results.add(i, result);
+      highscoreListListeners.forEach(notifyListener);
+    }
+
+    if(this.results.size() == oldSize) {
+      this.results.add(result);
+      highscoreListListeners.forEach(notifyListener);
+    }
+
+    if(this.results.size() > maxSize) {
+      this.results.remove(this.results.size() - 1);
+    }
+  }
+
+  public void addHighscoreListListener(HighscoreListListener listener) {
+    this.highscoreListListeners.add(listener);
+  }
+
+  public void removeHighscoreListListener(HighscoreListListener listener) {
+    this.highscoreListListeners.remove(listener);
+  }
+}
