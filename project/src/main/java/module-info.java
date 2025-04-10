b7d4560966880bcf91d4870651220f4b5aac4a9f
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-04-10 12:50:30 +0200

    Screw all yalls warnings

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -2,9 +2,10 @@ module ExpenseForm {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires com.github.f4b6a3.uuid;
     
     opens ExpenseForm to javafx.fxml;
     exports ExpenseForm;
+    exports ExpenseForm.model;
 }
